ae9b2dc5c1ba87b4570390557efee93970eca29d
Fernando da Silva Sousa <wild.nando@gmail.com>
2020-10-21 20:12:31 -0300

    Add elo credit card type based on regex found on https://gist.github.com/claudiosanches/26d9668f21dbdc787472

--- src/CreditCard.php
+++ src/CreditCard.php
@@ -25,6 +25,7 @@ class CreditCard
     public const TYPE_UNIONPAY           = 'unionpay';
     public const TYPE_VISA               = 'visa';
     public const TYPE_VISA_ELECTRON      = 'visa_electron';
+    public const TYPE_ELO                = 'elo';
 
     protected static $cards = [
         // Debit cards must come first, since they have more specific patterns than their credit-card equivalents.
@@ -58,6 +59,14 @@ class CreditCard
             'luhn'      => true,
         ],
         // Credit cards
+        self::TYPE_ELO               => [
+            'type'      => self::TYPE_ELO,
+            'pattern'   => '/^(40117[8-9]|431274|438935|451416|457393|45763[1-2]|506(699|7[0-6][0-9]|77[0-8])|509\d{3}|504175|627780|636297|636368|65003[1-3]|6500(3[5-9]|4[0-9]|5[0-1])|6504(0[5-9]|[1-3][0-9])|650(4[8-9][0-9]|5[0-2][0-9]|53[0-8])|6505(4[1-9]|[5-8][0-9]|9[0-8])|6507(0[0-9]|1[0-8])|65072[0-7]|6509(0[1-9]|1[0-9]|20)|6516(5[2-9]|[6-7][0-9])|6550([0-1][0-9]|2[1-9]|[3-4][0-9]|5[0-8]))/',
+            'format'    => '/(\d{1,4})(\d{1,6})?(\d{1,5})?/',
+            'length'    => [16],
+            'cvcLength' => [3, 4],
+            'luhn'      => true,
+        ],
         self::TYPE_AMEX               => [
             'type'      => self::TYPE_AMEX,
             'pattern'   => '/^3[47]/',
